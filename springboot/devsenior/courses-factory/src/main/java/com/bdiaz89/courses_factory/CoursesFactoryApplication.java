e4a0760c9eaaad7451a8c60fe62ac1a8eee43fdb
Artu <brayandiaz258f@gmail.com>
2025-11-11 21:02:14 -0500

    Add estructura nuevo proyecto a crear.

---
+++ springboot/devsenior/courses-factory/src/main/java/com/bdiaz89/courses_factory/CoursesFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.bdiaz89.courses_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesFactoryApplication.class, args);
+	}
+
+}
---
+++ springboot/devsenior/courses-factory/src/test/java/com/bdiaz89/courses_factory/CoursesFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdiaz89.courses_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
